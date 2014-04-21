6fe922d105af1a87d7e461bb663e3268d6b5eb8d
Chermaine Zimmerman <C14jcdj@yahoo.com>
2014-04-20 23:15:12 -0700

    funcitonal game

--- battleshipgame.rb
+++ battleshipgame.rb
@@ -130,6 +130,26 @@ class GameController
     attack
   end
 
+  def computer_attack
+    row = computer.board.row_decoder[('a'..'j').to_a.sample.upcase]
+    col = (1..10).to_a.sample
+    if player.board.board[row][col] == "*" || player.board.board[row][col] == "X"
+      puts "Computer hits your ship"
+      player.board.board[row][col] = 'X'
+      view.print_board(player.board)
+      check = check_board(player.board)
+      return "You Lose" if check == true
+    else
+      puts "Computer misses your ship"
+      player.board.board[row][col] = '/'
+      view.print_board(player.board)
+      check = check_board(player.board)
+      return "You Lose" if check == true
+    end
+
+
+  end
+
   def attack
     check = false
     until check
@@ -137,16 +157,18 @@ class GameController
     attack = gets.chomp
     row = computer.board.row_decoder[attack[0].upcase]
     col = attack.length == 3 ? 10 : attack[1].to_i
-    if computer.board.board[row][col] == "*"
+    if computer.board.board[row][col] == "*" || computer.board.board[row][col] == "X"
       puts "hit"
       computer.board.board[row][col] = "X"
       check = check_board(computer.board)
       view.print_board(computer.board)
+      computer_attack
     else
       puts "miss"
       computer.board.board[row][col] = "/"
       check = check_board(computer.board)
       view.print_board(computer.board)
+      computer_attack
     end
   end
   puts 'You Win!'
